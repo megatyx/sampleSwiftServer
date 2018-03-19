d0288e13cff9881399d32e9bff5175ff56c39b4a
TYLER <tyler@italki.com>
2018-03-19 12:52:35 +0900

    bug fixes

--- Package.swift
+++ Package.swift
@@ -1,20 +1,14 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// Generated automatically by Perfect Assistant 2
+// Date: 2018-03-16 10:41:45 +0000
 import PackageDescription
 
 let package = Package(
-    name: "MyAwesomeProject",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "MyAwesomeProject",
-            dependencies: ["PerfectHTTPServer"]),
-    ]
+	name: "MyAwesomeProject",
+	dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0")
+	],
+	targets: [
+		.target(name: "MyAwesomeProject", dependencies: ["PerfectHTTPServer"])
+	]
 )
